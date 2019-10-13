2538b93fdd6e982a0e7f31113956870c802efab8
pabra <pabra@netzfruech.de>
2019-10-13 20:09:34 +0200

    better return types for jsonify

--- src/jsonify.ts
+++ src/jsonify.ts
@@ -1,16 +1,51 @@
 import { PrimitiveObject, ArrayObject, ObjectObject } from './analyze';
 
-function jsonify(
-  thingObject: PrimitiveObject | ArrayObject | ObjectObject,
-): any {
+interface ArrayJson {
+  type: 'array';
+  path: string;
+  values: {};
+}
+
+interface ObjectJson {
+  type: 'object';
+  path: string;
+  keys: Record<string, {}>;
+}
+
+interface NullJson {
+  type: 'null';
+  path: string;
+}
+
+interface BooleanJson {
+  type: 'boolean';
+  path: string;
+  values: (true | false)[];
+}
+
+interface NumberJson {
+  type: 'number';
+  path: string;
+  values: number[];
+}
+
+interface StringJson {
+  type: 'string';
+  path: string;
+  values: string[];
+}
+
+function jsonify(thingObject: PrimitiveObject | ArrayObject | ObjectObject) {
   if (thingObject.type === 'array') {
-    return {
+    const arrayJson: ArrayJson = {
       type: thingObject.type,
       path: thingObject.path,
       values: Object.values(thingObject.values).map(v => jsonify(v)),
     };
+
+    return arrayJson;
   } else if (thingObject.type === 'object') {
-    return {
+    const objectJson: ObjectJson = {
       type: thingObject.type,
       path: thingObject.path,
       keys: Object.entries(thingObject.keys).reduce<Record<string, any>>(
@@ -27,16 +62,42 @@ function jsonify(
         {},
       ),
     };
-  } else {
-    const val: Record<string, any> = {
+
+    return objectJson;
+  } else if (thingObject.type === 'null') {
+    const nullJson: NullJson = {
       type: thingObject.type,
       path: thingObject.path,
     };
-    if (thingObject.type !== 'null') {
-      val.values = [...thingObject.values];
-    }
-    return val;
+
+    return nullJson;
+  } else if (thingObject.type === 'boolean') {
+    const booleanJson: BooleanJson = {
+      type: thingObject.type,
+      path: thingObject.path,
+      values: [...thingObject.values],
+    };
+
+    return booleanJson;
+  } else if (thingObject.type === 'number') {
+    const numberJson: NumberJson = {
+      type: thingObject.type,
+      path: thingObject.path,
+      values: [...thingObject.values],
+    };
+
+    return numberJson;
+  } else if (thingObject.type === 'string') {
+    const stringJson: StringJson = {
+      type: thingObject.type,
+      path: thingObject.path,
+      values: [...thingObject.values],
+    };
+
+    return stringJson;
   }
+
+  throw new Error();
 }
 
 export default jsonify;
